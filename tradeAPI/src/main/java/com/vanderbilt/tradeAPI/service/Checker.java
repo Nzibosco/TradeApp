415bd536cf8165388d3644d6fb68051ec33dddaa
Jean Bosco Nzeyimana <nzibosco@gmail.com>
2022-12-08 15:27:59 -0600

    commenting out unnecessary code

--- tradeAPI/src/main/java/com/vanderbilt/tradeAPI/service/Checker.java
+++ tradeAPI/src/main/java/com/vanderbilt/tradeAPI/service/Checker.java
@@ -1,20 +1,8 @@
 package com.vanderbilt.tradeAPI.service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Checker {
 
-    public static void main (String[] args){
-        Random rand = new Random();
-        double x = rand.nextDouble();
-        System.out.println(Double.toString(x).substring(2,6));
-        System.out.println(x);
-        BigDecimal bd = new BigDecimal(x).setScale(2, RoundingMode.HALF_EVEN);
-        System.out.println(bd.doubleValue());
-    }
+//    public static void main (String[] args){
+//
+//    }
 }
