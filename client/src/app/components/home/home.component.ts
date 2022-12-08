cf6af1d7101de9139fa13c532bde806df8a2c71d
Jean Bosco Nzeyimana <nzibosco@gmail.com>
2022-12-08 17:33:12 -0600

    removed facebook, added login token

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  tokenDiv: boolean = true;
+  token: string = 'dsgs';
+  tokenReady:boolean = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  unhide(){
+    this.tokenDiv = false;
+  }
+
+  setToken(token:string){
+    this.token = token;
+    console.log(this.token);
+    if(token.length > 4){
+      this.router.navigateByUrl('/dashboard');
+    }
   }
 
 }
